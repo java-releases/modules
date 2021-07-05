5aa8dfd01300074efaaafcc22b5cc63963ab777d
Luciano Ortiz <luciano.ortiz@neogrid.com>
2021-07-05 07:30:05 -0300

    Add modulo alimentacao

---
+++ alimentacao/src/module-info.java
@@ -0,0 +1,5 @@
+module modulo.alimentacao {
+
+	exports zoo.animal.alimentacao;
+
+}
---
+++ alimentacao/src/zoo/animal/alimentacao/Tarefa.java
@@ -0,0 +1,11 @@
+package zoo.animal.alimentacao;
+
+public class Tarefa {
+
+	public static void main(String[] args) {
+		System.err.println("------------------------------------------");
+		System.out.println("ALIMENTAR OS ANIMAIS TODOS OS DIAS AS 14H");
+		System.err.println("-------------------------------------------");
+	}
+
+}
